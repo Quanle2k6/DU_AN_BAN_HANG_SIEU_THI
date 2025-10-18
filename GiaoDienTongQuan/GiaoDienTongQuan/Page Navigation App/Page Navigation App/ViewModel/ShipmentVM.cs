33a9a32ae8a0769ac74be9847bcd2d1342b85df9
123456 <quanle2006295@gmail.com>
2025-10-19 00:29:38 +0700

    First Change

---
+++ GiaoDienTongQuan/GiaoDienTongQuan/Page Navigation App/Page Navigation App/ViewModel/ShipmentVM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Page_Navigation_App.Model;
+
+namespace Page_Navigation_App.ViewModel
+{
+    class ShipmentVM : Utilities.ViewModelBase
+    {
+        private readonly PageModel _pageModel;
+        public TimeOnly ShipmentTracking
+        {
+            get { return _pageModel.ShipmentDelivery; }
+            set { _pageModel.ShipmentDelivery = value; OnPropertyChanged(); }
+        }
+
+        public ShipmentVM()
+        {
+            _pageModel = new PageModel();
+           
+        }
+    }
+}
