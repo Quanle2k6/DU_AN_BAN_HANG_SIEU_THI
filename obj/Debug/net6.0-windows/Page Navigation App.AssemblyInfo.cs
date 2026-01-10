0a8232333b1fefb4006414c334141b15998f157d
https://github.com/Quanle2k6/BTH5_LeHoangQuan_24521432 <quanle2006295@gmail.com>
2026-01-10 23:19:57 +0700

    Change 10/01/2026 11:19

--- obj/Debug/net6.0-windows/Page Navigation App.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows/Page Navigation App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Page Navigation App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61a4bf6fa6a720979b0a791a585ba14c142133fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e2742fdf69e5202122c69f1a2dfdc3e0492cb50")]
 [assembly: System.Reflection.AssemblyProductAttribute("Page Navigation App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Page Navigation App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
