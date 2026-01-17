50a749460ef96e6aeda60b2324d6dba44e4621e4
Mai Phú Tân <pnat25092006@gmail.com>
2026-01-17 08:05:20 +0700

    Đổi UI cho bíu ti phô hơn

--- View/Orders.xaml.cs
+++ View/Orders.xaml.cs
@@ -1,4 +1,17 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace Page_Navigation_App.View
 {
@@ -7,6 +20,12 @@ namespace Page_Navigation_App.View
         public Orders()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var main = (MainWindow)Application.Current.MainWindow;
+            main.ShowOverlay(new PageCTHD());
         }
     }
 }
